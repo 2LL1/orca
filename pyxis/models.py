19de1e279070a0a5aff7dacfc176ba474cffcf1e
李丰 <madlee@lifengdeMac-mini.local>
2015-01-24 09:13:17 +0800

    Add pyxis

--- pyxis/models.py
+++ pyxis/models.py
@@ -8,42 +8,37 @@ from django.db import models, connection
 from django.contrib.auth.models import User
 from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
-
 @python_2_unicode_compatible
 class Account(models.Model):
-	name = models.CharField(max_length=200)
-	url = models.URLField()
-	username = models.CharField(max_length=50)
-	password = models.CharField(max_length=50)
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+    password = models.TextField()
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 
 @python_2_unicode_compatible
 class Command(models.Model):
-	account = models.ForeignKey(Account)
-	name = models.CharField(max_length=200)
-	command = models.TextField()
-	shell = models.TextField()
-	work_folder = models.TextField()
-	output = models.TextField()
+    account = models.ForeignKey(Account, related_name='commands')
+    name = models.CharField(max_length=200)
+    command = models.TextField()
+    work_folder = models.TextField()
 
-	def __str__(self):
-		return '%s on %s' % (self.name, self.account)
+    def __str__(self):
+        return '%s on %s' % (self.name, self.account)
 
 @python_2_unicode_compatible
 class JobLog(models.Model):
-	command = models.ForeignKey(Command)
-	author = models.ForeignKey(User)
+    command = models.ForeignKey(Command)
+    author = models.ForeignKey(User)
 
-	timestamp0 = models.DateTimeField(auto_now_add=True)
-	timestamp1 = models.DateTimeField(auto_now=True)
-	timestampZ = models.DateTimeField()
+    timestamp0 = models.DateTimeField(auto_now_add=True)
+    timestamp1 = models.DateTimeField(auto_now=True)
+    timestampZ = models.DateTimeField()
 
-	return_code = models.IntegerField(null=True, default=None)
+    return_code = models.IntegerField(null=True, default=None)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
