6e838d03340ce17ec6a10241948ed0916ebac2b2
李丰 <madlee@shujuzhongxin.local>
2015-01-25 20:03:35 +0800

    Change widgets in pyxis.admin

--- pyxis/admin.py
+++ pyxis/admin.py
@@ -2,6 +2,8 @@ from __future__ import unicode_literals
 from __future__ import division
 from __future__ import print_function
 
+from django.db import models
+from django.forms import TextInput
 from django.contrib import admin
 
 from pyxis.models import Account, Command
@@ -9,10 +11,16 @@ from pyxis.models import Account, Command
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'url', 'password')
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput},
+    }
 admin.site.register(Account, AccountAdmin)
 
 class CommandAdmin(admin.ModelAdmin):
     list_display = ('account', 'name', 'command', 'work_folder')
     list_display_links = ('name',)
+    formfield_overrides = {
+        models.TextField: {'widget': TextInput},
+    }
 admin.site.register(Command, CommandAdmin)
 
